c06b860b428b41ba40c973f00e5b9a7b94828117
Son <nguyennthanhson162001@gmail.com>
2021-07-31 14:06:52 +0700

    make it better

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose')
 async function connect() {
     try {
         const uri = "mongodb+srv://nguyenthanhson162001:Son162001@cluster1.rjvkr.mongodb.net/secondhand_books_store?retryWrites=true&w=majority";
-        // mongodb://cluster1-shard-00-01.rjvkr.mongodb.net:27017/secondhand_books_store
         await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
