2431b5ef6367a629eb413d7d904f88d0c136cd24
Son <nguyennthanhson162001@gmail.com>
2021-07-31 15:16:05 +0700

    v5BD

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 async function connect() {
-    const uri = "mongodb+srv://nguyenthanhson162001:Son162001@cluster1.rjvkr.mongodb.net/SecondHandBooksStore?retryWrites=true&w=majority"; try {
+
+    const uri = "mongodb+srv://nguyenthanhson162001:Son162001@secondhandbooks.nxpjh.mongodb.net/SecondHandBooksOnline?retryWrites=true&w=majority";
+    try {
         await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
