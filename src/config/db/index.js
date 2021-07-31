c9d3d9ef02daa9b02376fe4ab3261da6b9d05822
Son <nguyennthanhson162001@gmail.com>
2021-07-31 13:39:09 +0700

    db

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,16 +1,18 @@
 const mongoose = require('mongoose')
-async function connect(){
-    try{
-        await mongoose.connect('mongodb://localhost:27017/secondhand_books_store', {
+async function connect() {
+    try {
+        const uri = "mongodb+srv://nguyenthanhson162001:Son162001@cluster1.rjvkr.mongodb.net/secondhand_books_store?retryWrites=true&w=majority";
+        // mongodb://cluster1-shard-00-01.rjvkr.mongodb.net:27017/secondhand_books_store
+        await mongoose.connect(uri, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
             useCreateIndex: true
-          });
+        });
         console.log("connect DB successfully")
-    }catch(error){
-        console.log("connect DB failed")
+    } catch (error) {
+        console.log("connect DB failed" + error)
     }
 
 }
-module.exports = {connect}
+module.exports = { connect }
